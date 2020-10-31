68db0ba0b3288d0847f00e593236650ddff8d921
Furrybear <fbcll@outlook.com>
2020-10-31 23:16:25 +0800

    Some change.

---
+++ amend.js
@@ -0,0 +1,92 @@
+const child_process = require("child_process");
+const readline = require("readline");
+const fs = require('fs');
+const getFrontMatter = require('gray-matter');
+const { stringify: stringifyFrontMatter } = require('gray-matter');
+const path = require("path");
+const moment = require("moment");
+const { exit } = require("process");
+
+function amend(filePath) {
+    return new Promise((resolve, reject) => {
+        fs.readFile(filePath, 'utf8', function (err, data) {
+            console.log("\n#################################");
+            console.log(`Check and amend file ${filePath}`);
+            console.log("#################################");
+            if (err) {
+                reject(err);
+                return;
+            }
+            const frontMatter = getFrontMatter(data);
+            //console.log(frontMatter);
+            const { data: attrs } = frontMatter;
+
+            const fileState = fs.statSync(filePath);
+            const { mtime, birthtime } = fileState;
+
+            if (!attrs.title) {
+                if (filePath.match(/^.*\.[^\.]*$/))
+                    attrs.title = path.basename(filePath).replace(/^(.*)\.[^\.]*$/, "$1");
+                else
+                    attrs.title = path.basename(filePath);
+            }
+            if (!attrs.date) {
+                attrs.date = moment(birthtime).utc(8).format("YYYY-MM-DD HH:mm:ss");
+            } else {
+                attrs.date = moment(attrs.date).utc(0).format("YYYY-MM-DD HH:mm:ss");
+            }
+            attrs.updated = moment(mtime).utc(8).format("YYYY-MM-DD HH:mm:ss");
+
+            const output = stringifyFrontMatter(frontMatter)
+            console.log(output)
+
+            fs.writeFile(filePath, output, err => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve();
+            });
+        })
+    });
+}
+
+async function main() {
+    const argv = process.argv;
+
+    console.log("\n#################################");
+    console.log("The following files are in git staging area:");
+    console.log("#################################");
+    const filePaths = [];
+    //console.log(argv);
+    if (argv.length < 3) {
+        await new Promise((resolve, reject) => {
+            const p = child_process.exec("git diff --cached --name-only --diff-filter=ACMRTUXB source/_posts");
+            p.stderr.pipe(p.stdout);
+            const rl = readline.createInterface({ input: p.stdout });
+            rl.on("line", line => {
+                line = line.replace(/^"(.*)"$/, "$1");
+                console.log(line);
+                filePaths.push(line);
+            });
+            p.on('exit', (exitCode) => {
+                if (exitCode === 0) {
+                    resolve();
+                } else {
+                    reject(exitCode);
+                }
+            });
+        });
+    } else {
+        console.log(argv[2]);
+        filePaths.push(argv[2]);
+    }
+
+    const tasks = [];
+    for (const filePath of filePaths) {
+        tasks.push(amend(filePath));
+    }
+    await Promise.all(tasks);
+}
+
+main().catch(err => { console.error(err); exit(1) })
---
+++ source/_posts/1. Knowledge of Back End/HTTPS Basics.md
@@ -0,0 +1,31 @@
+---
+title: HTTPS基础知识
+date: '2020-10-29 10:03:47'
+updated: '2020-10-31 23:06:23'
+tags: null
+categories:
+  - 1. Knowledge of Back End
+keywords: null
+description: null
+top_img: null
+comments: null
+cover: null
+toc: null
+toc_number: null
+copyright: null
+copyright_author: null
+copyright_author_href: null
+copyright_url: null
+copyright_info: null
+mathjax: null
+katex: null
+aplayer: null
+aside: null
+---
+
+# HTTPS基础知识
+
+## TLS证书申请
+
+相关链接：[使用Let‘s Encrypt配置网站https](https://www.jianshu.com/p/ee5c589950d1)
+
